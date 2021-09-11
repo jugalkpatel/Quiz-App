32559c1eb9934f129c6ff667fe13af0047b25a1b
jugalkpatel <ptjimmy99@gmail.com>
2021-09-12 00:21:26 +0530

    style: added card wrapper and limit component wrapper width to 960px

--- src/pages/Main/main.styles.ts
+++ src/pages/Main/main.styles.ts
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const ComponentWrapper = styled.article`
   padding: 2rem 1rem;
+
+  @media (min-width: 960px) {
+    width: 960px;
+    margin: 0 auto;
+  }
 `;
 
 const PageHeader = styled.article`
@@ -18,4 +23,12 @@ const SubText = styled.p`
   margin: 0;
 `;
 
-export { ComponentWrapper, PageHeader, PageTitle, SubText };
+const CardWrapper = styled.article`
+  display: grid;
+
+  @media (min-width: 568px) {
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+`;
+
+export { ComponentWrapper, PageHeader, PageTitle, SubText, CardWrapper };
