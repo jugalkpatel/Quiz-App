0a4fff11a516406979ce9b0bedb932f770eaa346
jugalkpatel <ptjimmy99@gmail.com>
2021-09-17 22:50:53 +0530

    feat: added signup page

---
+++ src/pages/Signup/Signup.tsx
@@ -0,0 +1,5 @@
+function Signup() {
+  return <h1>This is Signup</h1>;
+}
+
+export { Signup };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,5 @@
 export { Home } from "./Home/Home";
 export { Main } from "./Main/Main";
+export { GamePlay } from "./GamePlay/GamePlay";
+export { Login } from "./Login/Login";
+export { Signup } from "./Signup/Signup";
