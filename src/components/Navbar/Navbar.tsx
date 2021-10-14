f18de528d5725f9245b731715a689a3929a09fdf
jugalkpatel <ptjimmy99@gmail.com>
2021-10-14 19:50:48 +0530

    refactor: added login button, user-info, SideMenu to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,63 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
 import { Logo } from "../Logo/Logo";
 import { TiWeatherNight } from "react-icons/ti";
 import { BiSun } from "react-icons/bi";
-import { StyledNavbar, Menu, DarkButton } from "./navbar.styles";
-import { WidthContainer } from "../../styles/common.styles";
+
+import {
+  StyledNavbar,
+  Menu,
+  DarkButton,
+  ButtonContainer,
+  UserName,
+  NavUserIcon,
+  Level,
+  LoginButton,
+} from "./navbar.styles";
+import { ColumnContainer, WidthContainer } from "../../styles/common.styles";
 
 import { ThemeProps } from "../../hooks/useDarkMode.hook";
+import { SideMenu } from "../index";
+import { useAuth } from "../../contexts";
 
 function Navbar({ mode, setMode }: ThemeProps) {
+  const [sidebarVisibility, setSideBarVisibility] = useState(false);
+  const credentials = useAuth();
   const navigate = useNavigate();
   return (
     <>
       <StyledNavbar>
         <WidthContainer>
-          <Menu onClick={() => navigate("/signup")} />
+          <Menu
+            onClick={() =>
+              setSideBarVisibility((prevState) => (prevState ? false : true))
+            }
+          />
           <Logo mode={mode} />
+
           <DarkButton onClick={() => setMode()}>
             {mode === "light" ? <TiWeatherNight /> : <BiSun />}
           </DarkButton>
+
+          {credentials?.authenticated ? (
+            <ButtonContainer>
+              <ColumnContainer>
+                <UserName>Jugal Patel</UserName>
+                <Level>Rookie</Level>
+              </ColumnContainer>
+
+              <NavUserIcon />
+            </ButtonContainer>
+          ) : (
+            <LoginButton onClick={() => navigate("/login")}>LOG IN</LoginButton>
+          )}
         </WidthContainer>
+
+        <SideMenu
+          visibility={sidebarVisibility}
+          setVisibility={setSideBarVisibility}
+        />
       </StyledNavbar>
     </>
   );
--- src/components/Navbar/navbar.styles.ts
+++ src/components/Navbar/navbar.styles.ts
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 import { CgMenu } from "react-icons/cg";
-import { zindex_one } from "../../styles/common.styles";
+import { TiUser } from "react-icons/ti";
+import {
+  zindex_one,
+  RowContainer,
+  primaryFont,
+} from "../../styles/common.styles";
 
 const StyledNavbar = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.border};
-
   color: ${({ theme }) => theme.tertiary};
   flex-shrink: 0;
   background-color: ${({ theme }) => theme.primary};
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
   z-index: ${zindex_one};
+  position: relative;
 `;
 
 const Menu = styled(CgMenu)`
@@ -24,14 +29,75 @@ const Menu = styled(CgMenu)`
 
 const DarkButton = styled.button`
   display: flex;
+  align-items: center;
   font-size: 2rem;
   color: ${({ theme }) => theme.tertiary};
   background: transparent;
   border: 0;
-
   @media (min-width: 568px) {
-    font-size: 1.5rem;
+    position: absolute;
+    top: 0.7rem;
+    right: 0;
+    padding: 0 1rem;
+    font-size: 1.6rem;
+  }
+
+  @media (min-width: 568px) and (max-width: 787px) {
+    top: 1.1rem;
   }
 `;
 
-export { StyledNavbar, Menu, DarkButton };
+const ButtonContainer = styled(RowContainer)`
+  align-items: center;
+  font-size: ${primaryFont};
+`;
+
+const UserName = styled.h5`
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.tertiary};
+`;
+
+const Level = styled.p`
+  padding: 0;
+  margin: 0;
+  color: ${({ theme }) => theme.tertiary};
+  font-size: 0.7rem;
+`;
+
+const NavUserIcon = styled(TiUser)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.tertiary};
+  margin-left: 0.5rem;
+`;
+
+const LoginButton = styled.button`
+  padding: 0.3rem 1rem;
+  border: 0;
+  padding: 0;
+  margin: 0 1rem;
+  color: ${({ theme }) => theme.tertiary};
+  background-color: transparent;
+  font-weight: 800;
+  font-family: ${primaryFont};
+  font-size: 1rem;
+  cursor: pointer;
+
+  @media (max-width: 568px) {
+    display: none;
+  }
+
+  @media (min-width: 568px) and (max-width: 1040px) {
+    margin: 0 2rem 0 0;
+  }
+`;
+
+export {
+  StyledNavbar,
+  Menu,
+  DarkButton,
+  ButtonContainer,
+  UserName,
+  NavUserIcon,
+  Level,
+  LoginButton,
+};
