053c37896525cc7e9c192b345f284f6c0012219d
jugalkpatel <ptjimmy99@gmail.com>
2021-10-15 22:53:43 +0530

    feat: added Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,73 @@
+import { Form, Formik } from "formik";
+import { useNavigate } from "react-router-dom";
+
+import {
+  FormContainer,
+  FormHeader,
+  FormLabel,
+  FormTitle,
+  FormLink,
+  SubmitButton,
+} from "../../styles/common.styles";
+
+import { LoginTypes } from "../../common";
+import { useAuth } from "../../contexts";
+import { InputField } from "../../components";
+import { loginSchema } from "../../validation";
+import { handleAuthClick } from "../../helpers";
+
 function Login() {
-  return <h1>Hello, this is login</h1>;
+  const initialValues: LoginTypes = {
+    email: "",
+    password: "",
+  };
+  const { setAuthCredentials } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSumit = (values: typeof initialValues) => {
+    const url = "/auth/login";
+    handleAuthClick({ values, setAuthCredentials, navigate, url });
+  };
+
+  return (
+    <FormContainer>
+      <FormHeader>
+        <FormTitle>log in</FormTitle>
+        <FormLabel>
+          Not registered?
+          <FormLink to="/signup">sign up</FormLink>
+        </FormLabel>
+      </FormHeader>
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={handleSumit}
+      >
+        {() => (
+          <Form>
+            <InputField
+              label="email"
+              id="email"
+              type="email"
+              value="email"
+              placeholder="Enter Email"
+            />
+
+            <InputField
+              label="Password"
+              id="password"
+              type="password"
+              value="password"
+              placeholder="Enter Password"
+            />
+
+            <SubmitButton type="submit">log in</SubmitButton>
+          </Form>
+        )}
+      </Formik>
+    </FormContainer>
+  );
 }
 
 export { Login };
