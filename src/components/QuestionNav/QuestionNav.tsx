8e7766bbe3799886d851b8f74c976447c59dbf57
jugalkpatel <ptjimmy99@gmail.com>
2021-10-28 18:05:42 +0530

    refactor: changed NumberLabel key prop

--- src/components/QuestionNav/QuestionNav.tsx
+++ src/components/QuestionNav/QuestionNav.tsx
@@ -4,8 +4,8 @@ function QuestionNav() {
   const questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <QuestionNavigation>
-      {questions.map((number: number, index: number) => (
-        <NumberLabel key={index}>{number}</NumberLabel>
+      {questions.map((number: number) => (
+        <NumberLabel key={number}>{number}</NumberLabel>
       ))}
     </QuestionNavigation>
   );
