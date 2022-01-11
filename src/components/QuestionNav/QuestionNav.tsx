791b8b7147bbcb633321453ab77c0f0fe96d4238
jugalkpatel <ptjimmy99@gmail.com>
2022-01-11 23:52:46 +0530

    refactor: add level, isSubmitted props and extracted onclick method

--- src/components/QuestionNav/QuestionNav.tsx
+++ src/components/QuestionNav/QuestionNav.tsx
@@ -1,28 +1,36 @@
 import { QuestionNavigation, NumberLabel } from "./QuestionNav.styles";
-import { QuestionType } from "../../common";
-import { ACTIONS } from "../../helpers";
+
+import { LevelTypes, QuestionType } from "../../common";
 import { ACTIONTYPE } from "../../hooks/useGamePlay/useGamePlay.hook";
+import { ACTIONS } from "../../helpers";
 
 export type QuestionNavProps = {
+  level: LevelTypes;
+  isSubmitted: boolean;
   questionNumber: number;
   questions: QuestionType[];
   dispatch: React.Dispatch<ACTIONTYPE>;
 };
 
-// TODO: Highlight Question Number based on current Question
 function QuestionNav({
+  level,
+  isSubmitted,
   questionNumber,
   questions,
   dispatch,
 }: QuestionNavProps) {
+  const handleClick = (index: number) => {
+    return level !== "Rookie" && !isSubmitted
+      ? null
+      : dispatch({ type: ACTIONS.REDIRECT_TO, payload: { to: index } });
+  };
+
   return (
     <QuestionNavigation>
       {questions.map(({ id }, index) => (
         <NumberLabel
           key={id}
-          onClick={() =>
-            dispatch({ type: ACTIONS.REDIRECT_TO, payload: { to: index } })
-          }
+          onClick={() => handleClick(index)}
           enableHighlight={index === questionNumber ? true : false}
         >
           {index + 1}
