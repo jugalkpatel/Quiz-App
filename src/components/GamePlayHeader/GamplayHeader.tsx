2b6fe1cf064d1e2fd0915d2aa2e13f27509c20a0
jugalkpatel <ptjimmy99@gmail.com>
2022-01-11 23:37:46 +0530

    feat: add timer

--- src/components/GamePlayHeader/GamplayHeader.tsx
+++ src/components/GamePlayHeader/GamplayHeader.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 import {
   GamePlayHeaderContainer,
   QuitButton,
@@ -9,17 +12,62 @@ import {
   CommonWrapper,
 } from "./GamePlayHeader.styles";
 
-function GamePlayHeader() {
+import { LevelTypes } from "../../common";
+import { ACTIONTYPE } from "../../hooks/useGamePlay/useGamePlay.hook";
+import { ACTIONS } from "../../helpers";
+
+export type GamePlayHeaderProps = {
+  isSubmitted: boolean;
+  level: LevelTypes;
+  questionNumber: number;
+  dispatch: React.Dispatch<ACTIONTYPE>;
+};
+
+function GamePlayHeader({
+  isSubmitted,
+  level,
+  questionNumber,
+  dispatch,
+}: GamePlayHeaderProps) {
+  const [time, setTime] = useState(30);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (level === "Rookie" || isSubmitted) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      dispatch({
+        type: ACTIONS.SUBMIT_ANSWER,
+        payload: { questionNumber, userAnswerIndex: -1, isCorrect: false },
+      });
+    }, 30000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+      setTime(30);
+    };
+  }, [questionNumber, dispatch, level, isSubmitted]);
+
   return (
     <GamePlayHeaderContainer>
-      <QuitButton>
+      <QuitButton onClick={() => navigate(-1)}>
         <QuitIcon />
       </QuitButton>
 
-      <CommonWrapper>
-        <TimeIcon />
-        <TimeLabel>10</TimeLabel>
-      </CommonWrapper>
+      {level !== "Rookie" && (
+        <CommonWrapper>
+          <TimeIcon />
+          <TimeLabel>{time}</TimeLabel>
+        </CommonWrapper>
+      )}
 
       <CommonWrapper>
         <ScoreIcon />
