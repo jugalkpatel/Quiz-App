cbc7b4a03f90a2cb925f00a58e384f0a6ecf956c
jugalkpatel <ptjimmy99@gmail.com>
2022-01-25 22:44:17 +0530

    fix: add return in setupAuthHeader

--- src/utils/setupAuthHeader.ts
+++ src/utils/setupAuthHeader.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 const setupAuthHeader = (token: string) => {
   if (token) {
     axios.defaults.headers.common["Authorization"] = token;
+    return;
   }
   delete axios.defaults.headers.common["Authorization"];
 };
